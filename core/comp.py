23a92237a723a08c85be5b25c4745b0f8f9e6f4c
kanataso <kanapo.go@gmail.com>
2024-04-11 02:50:08 +0900

    fix: change case expr to stmt
    veryl emits conditional operator "?" for case expression.
    So I can't use case expression to implement decoder that uses don't care.

--- core/comp.py
+++ core/comp.py
@@ -1,20 +1,12 @@
-
-CASEX_LINT_OFF = "/* verilator lint_off CASEX */\n"
-CASEX_LINT_ON  = "/* verilator lint_on CASEX */\n"
-CASEX = "casex"
-
 fileName = "src/decoder.sv"
-
 
 S = None
 with open(fileName, mode="r") as f:
     S = f.readlines()
 
 with open(fileName, mode="w") as f:
-    f.write(CASEX_LINT_OFF)
     for i in range(len(S)):
         s = S[i]
-        s = s.replace("case (", "casex (")
+        s = s.replace("case (", "casez (")
         S[i] = s
         f.write(s)
-    f.write(CASEX_LINT_ON)
