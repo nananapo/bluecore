467e0f64e505d295cfcd8f8ce8824f07588a919b
nananapo <kanapo.go@gmail.com>
2025-05-20 21:41:23 +0900

    book/12/debugout-range

--- core/src/tb_verilator.cpp
+++ core/src/tb_verilator.cpp
@@ -52,13 +52,15 @@ int main(int argc, char** argv) {
     setenv("ROM_FILE_PATH", rom_file_path.c_str(), 1);
     setenv("RAM_FILE_PATH", ram_file_path.c_str(), 1);
 
+    // #@@range_begin(set)
     // デバッグ用の入出力デバイスのアドレスを取得する
-    const char* dbg_addr_c = getenv("DBG_ADDR");
-    const unsigned long long DBG_ADDR = dbg_addr_c == nullptr ? 0 : std::strtoull(dbg_addr_c, nullptr, 0);
+    @<b>|const char* dbg_addr_c = getenv("DBG_ADDR");|
+    @<b>|const unsigned long long DBG_ADDR = dbg_addr_c == nullptr ? 0 : std::strtoull(dbg_addr_c, nullptr, 0);|
 
     // top
     Vcore_top *dut = new Vcore_top();
-    dut->MMAP_DBG_ADDR = DBG_ADDR;
+    @<b>|dut->MMAP_DBG_ADDR = DBG_ADDR;|
+    // #@@range_end(set)
 
     // reset
     dut->clk = 0;
